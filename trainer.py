047a2fbc4597a8b28fdd79b455ece1e0f8c18686
ammarnasr <ammarnasraza@gmail.com>
2020-06-21 02:16:49 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -51,7 +51,7 @@ class Trainer(object):
         image_encoder = CNN_ENCODER(cfg.TEXT.EMBEDDING_DIM)
         img_encoder_path = cfg.TRAIN.NET_E.replace('text_encoder', 'image_encoder')
         state_dict = torch.load(img_encoder_path, map_location=lambda storage, loc: storage)
-        #image_encoder.load_state_dict(state_dict)
+        image_encoder.load_state_dict(state_dict)
         for p in image_encoder.parameters():
             p.requires_grad = False
         print('Load image encoder from:', img_encoder_path)
@@ -59,7 +59,7 @@ class Trainer(object):
 
         text_encoder = RNN_ENCODER(self.n_words, nhidden=cfg.TEXT.EMBEDDING_DIM)
         state_dict = torch.load(cfg.TRAIN.NET_E, map_location=lambda storage, loc: storage)
-        #text_encoder.load_state_dict(state_dict)
+        text_encoder.load_state_dict(state_dict)
         for p in text_encoder.parameters():
             p.requires_grad = False
         print('Load text encoder from:', cfg.TRAIN.NET_E)
